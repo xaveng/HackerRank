ccc694e9f9833e307d63207cc1fd008ee6c69c0a
Seo Jeong <xaveng@gmail.com>
2016-01-16 13:10:08 +0900

    Repackaging

---
+++ Day0/src/com/github/xaveng/hackerrank/HelloWorld.java
@@ -0,0 +1,33 @@
+package com.github.xaveng.hackerrank;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+/**
+ * Welcome to Day 0 and the wonderful world of coding! Check out the video
+ * tutorial here, or just jump right into the problem. The tutorial is based on
+ * Java, but you can submit your solution in other popular languages.
+ * 
+ * For this challenge, all you have to do is print the following two lines:
+ * 
+ * <pre>
+ * Hello World. 
+ * Welcome to 30 Days of Code.
+ * </pre>
+ * 
+ * You got this!
+ */
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+
+		// Write your print statements here!!
+		// Template: System.out.println("what you want to print");
+		System.out.println("Hello World.");
+		System.out.println("Welcome to 30 Days of Code.");
+	}
+}
